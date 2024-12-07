b44d3ca8e54d0331991ec1fd98deb0b1fbea4e57
awesomebruh03 <2001sajidullah@gmail.com>
2024-12-07 20:39:50 +0600

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="text-8xl text-blue-950">React TypeScript App</h1>
+      </header>
+    </div>
+  )
+}
+
+export default App
